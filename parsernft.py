9d11845756a41f3b164c5d968b417952f0d1c95d
Узбек <danyachuglaev@gmail.com>
2026-07-09 13:11:45 +0300

    Update parsernft.py

--- parsernft.py
+++ parsernft.py
@@ -44,7 +44,7 @@ ADMIN_ID = 8986358602
 
 REQUIRED_CHANNEL = "@qwertyyyyw"
 REQUIRED_CHANNEL_URL = "https://t.me/qwertyyyyw"
-MONITOR_CHAT_ID = -1004306033280
+MONITOR_CHAT_ID = -1004439671296
 
 SESSION_NAME = "telethon_market_userbot"
 
